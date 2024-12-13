ea14e0ddb23f0169f6438becd514d2c9b8eedc80
Alexader Quejada cordoba <quejadacordobaalexander@gmail.com>
2024-12-13 14:02:05 +0000

    metodos-starwars

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
+
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +45,202 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+#listar usuarios
+@app.route('/users', methods=['GET'])
+def list_users():
+    try:
+        # aqui estoy con sultado  todos los registros de la tabla User
+        users = User.query.all()
+        print(users)
+        if not users:
+            raise APIException("No hay registros en la base de datos", status_code=404)
+        
+        # aqui serializo los datos usando el método `serialize` definido en el modelo
+        return jsonify([user.serialize() for user in users]), 200
+    except Exception as e:
+        # En caso de error, lanzar una excepción con el mensaje
+        raise APIException(str(e))
+    
+
+#listar people
+@app.route('/people', methods=['GET'])
+def list_people():
+    try:
+        people = People.query.all()
+        if not people:
+            raise APIException("no hay regstros de personas en la base de datos", status_code=404)
+        return jsonify([person.serealize() for person in people]), 200
+    except Exception as e:
+        raise APIException(str(e)) 
+       
+#identificar people por su id
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person_by_id(people_id):
+    try:
+        #consulta un people por su id
+        person = People.query.get(people_id)
+
+        if not person:
+            raise APIException("Persona con ID {people_id} no encontrado", status_code=404)
+        #serializar y devolver el usuario
+        return jsonify(person.serialize()), 200
+    except Exception as e:
+        #menejo de error
+        raise APIException(str(e))
+
+#listar planetas
+@app.route('/planets', methods=['GET'])
+def list_planets():
+    try:
+        planets = Planet.query.all()
+
+        if not planets:
+            raise APIException("No hay registros de planetas en la base de datos", status_code=404)
+        return jsonify([planet.serialize() for planet in planets]), 200
+    except Exception as e:
+        raise APIException(str(e))     
+
+#identificar planetas por su id
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+
+        if not planet:
+            raise APIException(f"Planeta con ID {planet_id} no encontrado", status_code=404)
+        return jsonify(planet.serialize()), 200
+    except Exception as e:
+        raise APIException(str(e))
+
+#asociar un usuario con favorito    
+@app.route('/users/<int:user_id>/favorites', methods=['GET']) 
+def get_user_favorites(user_id):
+    try:
+        user = User.query.get(user_id)
+        if not user:
+            raise APIException(f"Usuario con ID {user_id} no encontrado", status_code=404)
+        
+        favorites = Favorite.query.filter_by(user_id=user_id).all()
+        if not favorites:
+            raise APIException(f"No se encontraron favoritos para el usuario con ID {user_id}", status_code=404)
+        
+        return jsonify([favorite.serialize() for favorite in favorites]), 200
+    except Exception as e:
+        raise APIException(str(e))
+    
+
+#asociar un planeta con favorito
+@app.route('/favorites/planets/<int:planet_id>/user/<int:user_id>', methods=['POST'])
+def add_favorite_planet(user_id, planet_id):
+    try:
+        #virifica si el usuario existe
+        user = User.query.get(user_id)
+        if not user:
+            raise APIException(f"Usuario con ID {user_id} no encontrado",status_code=404)
+
+        #verifica si el planeta existe
+        planet = Planet.query.get(planet_id)
+        if not planet:
+            raise APIException(f"Planeta con ID {planet_id} no encontrado", status_code=404)
+        
+        #verifica si el favorito ya existe
+        existing_favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+        if existing_favorite:
+            raise APIException(f"El planeta con ID {planet_id} ya está en los favoritos del usuario", status_code=400)
+
+        #create un nuevo favorito
+        new_favorite = Favorite(user_id=user_id, planet_id=planet_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+
+
+        return jsonify({
+            "msg": f"Planeta con ID {planet_id} añadido a los favoritos del usuario con ID {user_id}"
+        }),201
+    except Exception as e:
+        raise APIException(str(e))
+
+
+#asociar people con favorito
+@app.route('/favorites/people/<int:people_id>/user/<int:user_id>', methods=['POST'])
+def add_favorite_people(user_id, people_id):
+    try:
+        #verifico si el usuario existe
+        user = User.query.get(user_id)
+        if not user:
+            raise APIException(f"Usuario con ID {user_id} no encontrado",status_code=404)
+        #verifico si el personaje existe
+        people = People.query.get(people_id)
+        if not people:
+            raise APIException(f"Personaje con ID {people_id} no encontrado", status_code=404)
+
+        #verifacar si el favorito ya existe
+        existing_favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+        if existing_favorite:
+            raise APIException(f"El personaje con ID {people_id} ya está en los favoritos del usuario con ID {user_id}", status_code=400)
+
+        #creo un nuevo favorito
+        new_favorite = Favorite(user_id=user_id, people_id=people_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify({
+            "msg": f"People con ID {people_id} añadido a los favoritos del usuario con ID {user_id}"
+        }),201
+    except Exception as e:
+        raise APIException(str(e)) 
+    
+#delete people
+@app.route('/favorites/people/<int:people_id>/user/<int:user_id>', methods=['DELETE'])
+def delete_favorite_people(user_id, people_id):
+    try:
+        #verifico si existe el usurio
+        user = User.query.get(user_id)
+        if not user:
+            raise APIException(f"Usuario con ID {user_id} no encontrado", status_code=404)
+
+        #verifico si el personaje favorito existe para este usuario
+        favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first() 
+        if not favorite:
+            raise APIException(f"El personaje con ID {people_id} no está en los favoritos del usuario con ID {user_id}", status_code=404)
+
+        #elimino favorito
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({
+            "msg": f"Personaje con ID {people_id} eliminado de los favoritos del usuario con ID {user_id}"
+        }),200
+    except APIException as e:
+        raise APIException(str(e))
+
+#delete planet 
+@app.route('/favorites/planets/<int:planet_id>/user/<int:user_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id, planet_id):
+    try:
+        #vericar si existe el usuario
+        user = User.query.get(user_id)
+        if not user:
+            raise APIException(f"Usuario con ID {user_id} no encontrado", status_code=404)
+
+
+        #verifacamos si el planeta existe para este usuario
+        favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+        if not favorite:
+            raise APIException(f"El planeta con ID {planet_id} no está en los favoritos del usuario con ID {user_id}", status_code=404)
+
+        #eliminar el favorito de la base de datos
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({
+            "msg": f"Planeta con ID {planet_id} eliminado de los favoritos del usuario con ID {user_id}"
+
+        }),200
+    except APIException as e:
+         raise APIException(str(e))
+       
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -7,13 +8,101 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', back_populates='user', cascade='all, delete')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            
             # do not serialize the password, its a security breach
         }
+        #tabala people
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    height = db.Column(db.String(50))
+    mass = db.Column(db.String(50))
+    hair_color = db.Column(db.String(50))
+    ski_colo = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    birth_year = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    homeworld = db.Column(db.String(120))
+    description = db.Column(db.Text, nullable=True)
+    favorites = db.relationship('Favorite', back_populates='people', cascade='all, delete')  
+
+    def __repr__(self):
+        return f'<Peole {self.name}>'      
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.ski_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+            "description": self.description,
+            
+        }
+#tabla planet
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(50))
+    terrain = db.Column(db.String(50))
+    population = db.Column(db.String(50))
+    diameter = db.Column(db.String(50))
+    rotation_period = db.Column(db.String(50))
+    orbital_period = db.Column(db.String(50))
+    favorites = db.relationship('Favorite', back_populates='planet', cascade='all, delete')
+
+    def __repr__(self):
+        return f'<planet {self.name}>'
+
+    def serialize(self):
+        return{
+         "id": self.id,
+        "name": self.name,
+        "climate": self.climate,
+        "terrain": self.terrain,
+        "population": self.population,
+        "diameter": self.diameter,
+        "rotation_period": self.rotation_period,
+        "orbital_period": self.orbital_period,
+
+        }
+
+ #tabla favorite   
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    user = db.relationship('User', back_populates='favorites')
+    people = db.relationship('People', back_populates='favorites')
+    planet = db.relationship('Planet', back_populates='favorites')
+
+    def __repr__(self):
+        return f'<Favorite User: {self.user_id}, People: {self.people_id}, Planet: {self.planet_id} >'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user_id,
+            "people": self.people_id,
+            "planet": self.Planet_id,
+
+        }
+            
